364b21ddf989657f8cb992de9fdb16ac0c9243cc
AlexanderM1927 <52513621+AlexanderM1927@users.noreply.github.com>
2020-03-26 19:00:19 -0500

    WebApp V0
    Ready to coder

---
+++ www/app/Http/Controllers/WebsiteController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class WebsiteController extends Controller
+{
+    /**
+     * undocumented function
+     *
+     * @return void
+     * @author 
+     **/
+    public function index ()
+    {
+        return view('index');
+    }
+}
