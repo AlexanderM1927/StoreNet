18bd063c6b6bc0559f2e7948e396044c39651083
AlexanderM1927 <52513621+AlexanderM1927@users.noreply.github.com>
2020-03-26 19:22:26 -0500

    Sincronización de ramas

---
+++ www/resources/views/index.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>StoreNet</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="css/uikit.min.css" />
+        <link rel="stylesheet" href="{{ mix('css/app.css') }}" />
+        <script src="js/uikit.min.js"></script>
+        <script src="js/uikit-icons.min.js"></script>
+        <meta http-equiv="X-UA-Compatible" content="IE=EmulateIE11" />
+    </head>
+    <body>
+    Bienvenido a StoreNet
+        <script src="{{ mix('js/app.js') }}"></script>
+    </body>
+</html>
