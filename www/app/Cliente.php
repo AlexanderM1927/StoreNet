65c77a8e1b16c8c80dc35fccfac40dddfac6beb0
AlexanderM1927 <52513621+AlexanderM1927@users.noreply.github.com>
2020-03-29 23:24:14 -0500

    Agregar clientes 100%

--- www/app/Cliente.php
+++ www/app/Cliente.php
@@ -12,9 +12,20 @@ class Cliente
         return json_encode($arrayClientes);
     }
 
-    public function listarClientes_parametro($param){
-        $arrayClientes = DB::select("SELECT * FROM cliente WHERE nombres LIKE '%$param%' LIMIT 10");
-        return json_encode($arrayClientes);
+    public function listarClientes_parametro($param,$data){
+        if($param == 'nombres' || $param == 'apellidos'){
+            $arrayClientes = DB::select("SELECT * FROM cliente WHERE CONCAT(nombres,' ',apellidos) LIKE '%".$data."%'");
+            return json_encode($arrayClientes);
+        }else{
+            $arrayClientes = DB::select("SELECT * FROM cliente WHERE ".$param." LIKE '%".$data."%' LIMIT 20");
+            return json_encode($arrayClientes);
+        }
+    }
+
+    public function registrarCliente($nombres,$apellidos,$mail,$password,$direccion,$telefono)
+    {
+        DB::insert('INSERT INTO cliente (nombres, apellidos, mail, password, direccion, telefono) values (?, ?, ?, ?, ?, ?)', [ $nombres,
+         $apellidos, $mail, $password, $direccion, $telefono]);
     }
     
 
--- www/resources/js/components/cliente.vue
+++ www/resources/js/components/cliente.vue
@@ -4,15 +4,14 @@
     ¿Deseas filtrar la tabla?:
                 <div class="uk-inline">
                 <span class="uk-form-icon" uk-icon="icon: cog"></span>
-                <input class="uk-input field" type="text" placeholder="Busqueda..." required>
+                <input class="uk-input field" v-model="datos" v-on:keyup="buscar" type="text" placeholder="Busqueda..." required>
                 </div>
                 <div class="uk-inline">
                 <span class="uk-form-icon" uk-icon="icon: tag"></span>
-                <select class="uk-input field" required>
-                  <option value="0">id</option>
-                  <option value="1">nombres</option>
-                  <option value="2">apellidos</option>
-                  <option value="3">mail</option>
+                <select v-on:change="buscar" class="uk-input field" v-model="tipo">
+                  <option value="id">id</option>
+                  <option value="nombres">nombres</option>
+                  <option value="mail">mail</option>
                 </select>
                 </div>
     <div class="uk-overflow-auto tabla-clientes barra">
@@ -41,6 +40,61 @@
         </tbody>
     </table>
     </div>
+    <center>
+      <br><br>
+      <button class="uk-button uk-button-primary uk-margin-small-right" type="button" uk-toggle="target: #modal-center">{{metodo}}</button>
+    </center>
+        <div id="modal-center" class="uk-flex-top" uk-modal>
+        <div class="creadorForm">
+      <h3 class="title">{{metodo}}</h3>
+      <div class="content">
+        <center>
+        <form action="procesarClientes/1" method="POST">
+        <input type="hidden" name="_token" :value="csrf">
+        <div class="uk-margin">
+          <div class="uk-inline">
+            <span class="uk-form-icon" uk-icon="icon: user"></span>
+            <input class="uk-input field" type="text" name="nombres" placeholder="Nombres" required>
+          </div>
+        </div>
+        <div class="uk-margin">
+          <div class="uk-inline">
+            <span class="uk-form-icon" uk-icon="icon: user"></span>
+            <input class="uk-input field" type="text" name="apellidos" placeholder="Apellidos" required>
+          </div>
+        </div>
+        <div class="uk-margin">
+          <div class="uk-inline">
+            <span class="uk-form-icon" uk-icon="icon: mail"></span>
+            <input class="uk-input field" type="mail" name="mail" placeholder="Correo eléctronico" required>
+          </div>
+        </div>
+        <div class="uk-margin">
+          <div class="uk-inline">
+            <span class="uk-form-icon" uk-icon="icon: lock"></span>
+            <input class="uk-input field" type="password" name="password" placeholder="Clave" required>
+          </div>
+        </div>
+        <div class="uk-margin">
+          <div class="uk-inline">
+            <span class="uk-form-icon" uk-icon="icon: location"></span>
+            <input class="uk-input field" type="text" name="direccion" placeholder="Direccion" required>
+          </div>
+        </div>
+        <div class="uk-margin">
+          <div class="uk-inline">
+            <span class="uk-form-icon" uk-icon="icon: phone"></span>
+            <input class="uk-input field" type="text" name="telefono" placeholder="Telefono" required>
+          </div>
+        </div>
+        <button class="uk-button uk-button-default">Agregar</button>
+        </form>
+        <button class="uk-modal-close-default" type="button" uk-close>Cerrar</button>
+        </center>
+      </div>
+    </div>
+        </div>
+        </div>
   </div>
 </template>
 
@@ -52,15 +106,25 @@ export default {
   data() {
     return {
         nombre:'',
-        clientes: []
+        clientes: [],
+        datos: '',
+        tipo: 'nombres',
+        csrf: document.querySelector('meta[name="csrf-token"]').getAttribute('content'),
+        metodo: 'Agregar cliente'
     }
   },
   mounted(){
     axios
-      .get('../procesarClientes/0/0')
+      .get('../procesarClientes/0')
       .then(response => (this.clientes = response.data))
   },
   methods: {
+    buscar()
+    {
+      axios
+      .get('../procesarClientes/0/'+this.tipo+'/'+this.datos) //Filtros
+      .then(response => (this.clientes = response.data))
+    }
   }
 };
 </script>
